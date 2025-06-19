f907cd3c1ea2d763f883cfcdcd0a412d18aa93a3
Darshana P.S <151154925+Dharshanapalanisamy@users.noreply.github.com>
2025-06-19 18:38:46 +0530

    Create sum of array.java

---
+++ sum of array.java
@@ -0,0 +1,11 @@
+public class sumofarray {
+    public static void main(String[] args) {
+        int[] Array = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int num : Array) 
+        { 
+            sum += num;
+        }
+        System.out.println(+ sum);
+    }
+}
