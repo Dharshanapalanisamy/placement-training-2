46e828948f244c6c41d5c40158e5f70e0c3149f9
Darshana P.S <151154925+Dharshanapalanisamy@users.noreply.github.com>
2025-06-19 18:49:40 +0530

    Create swapping of numbers.java

---
+++ swapping of numbers.java
@@ -0,0 +1,11 @@
+public class swapping {
+    public static void main(String[] args)
+    {
+        int x = 10, y = 20;
+        int temp = x;
+        x = y;
+        y = temp;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
